595fabb049af6be443f7e704a2158343e2156672
knschneider <45770452+kevsch88@users.noreply.github.com>
2025-07-22 16:23:40 -0700

    ColorSet class for handling mapping of colors to metric arrays for a given group

--- oss_app/plotting.py
+++ oss_app/plotting.py
@@ -15,78 +15,127 @@ import pandas as pd
 import marimo as mo
 import numpy as np
 
-
-def map_colors(self, data_array: np.ndarray = [], color_set=["viridis_r", "plasma_r"], cmap_rng=None):
-    if not data_array:
-        data_array = self.si_scores
-
-    self.cset = color_set
-    num_sex = len(np.unique(self.df["Sex"].values))
-
-    if not cmap_rng:
-        cmap_rng = (0.4, 1)
-
-    match num_sex:
-        case 2:  # 2 sexes present
-            # default colors for male and female animals
-            plot_colors = []
-            self.c_debug(
-                f"using {self.cset} as color sets for 2 groups (default:sexes)")
-            for i, cset in enumerate(self.cset):
-                plot_colors.append(
-                    CmapSet(cset, data_array, cmap_range=cmap_rng))
-        # only males present
-        case 1 if "Male".casefold() in self.df["Sex"].values:
-            plot_colors = CmapSet(
-                self.cset[0], data_array, cmap_range=cmap_rng)
-            self.c_debug(f"using {self.cset[0]} as color set for males")
-        # only females present
-        case 1 if "Female".casefold() in self.df["Sex"].values:
-            plot_colors = CmapSet(
-                self.cset[1], data_array, cmap_range=cmap_rng)
-            self.c_debug(f"using {self.cset[1]} as color set for females")
-        case _:  # default to plasma cmap in any other case
-            plot_colors = CmapSet(
-                self.cset[0], data_array, cmap_range=cmap_rng)
-            self.c_debug(f"using {self.cset[1]} as default color set")
-
-    self.plot_colors = plot_colors
+from oss_app.utils import mo_print
 
 
-def blend_cmap(self, name, split=True, cmap_rng: tuple[float, float] = None, col_choice=None):
-    # for corr heatmaps (pca), makes it one directional - might change this
-    if col_choice is None:
-        col_choice = self.color_choice
-    match col_choice:
-        case "viridis_r":
-            tophalf, bothalf = "BrBG_r", "BrBG"
-        case "plasma_r":
-            tophalf, bothalf = "RdPu_r", "RdPu"
-        case _:
-            if "_r" in col_choice:
-                tophalf, bothalf = f"{col_choice[:-2]}", f"{col_choice}"
-            else:
-                tophalf, bothalf = f"{col_choice}_r", f"{col_choice}"
+# def map_colors(self, data_array: np.ndarray = [], color_set=["viridis_r", "plasma_r"], cmap_rng=None):
+#     if not data_array:
+#         data_array = self.si_scores
 
-    top = cm.get_cmap(tophalf, 1024)
-    bottom = cm.get_cmap(bothalf, 1024)
+#     self.cset = color_set
+#     num_sex = len(np.unique(self.df["Sex"].values))
 
-    if not cmap_rng:
-        cmap_rng = (0, 1)
-    if split:
-        newcolors = np.vstack(
-            (top(np.linspace(0, 0.5, 1024)), bottom(np.linspace(0.5, 1, 1024))))
-    else:
-        newcolors = np.vstack(
-            (
-                top(np.linspace(1 - cmap_rng[1], 1 - cmap_rng[0], 1024)),
-                bottom(np.linspace(cmap_rng[0], cmap_rng[1], 1024)),
+#     if not cmap_rng:
+#         cmap_rng = (0.4, 1)
+
+#     match num_sex:
+#         case 2:  # 2 sexes present
+#             # default colors for male and female animals
+#             plot_colors = []
+#             self.c_debug(
+#                 f"using {self.cset} as color sets for 2 groups (default:sexes)")
+#             for i, cset in enumerate(self.cset):
+#                 plot_colors.append(
+#                     CmapSet(cset, data_array, cmap_range=cmap_rng))
+#         # only males present
+#         case 1 if "Male".casefold() in self.df["Sex"].values:
+#             plot_colors = CmapSet(
+#                 self.cset[0], data_array, cmap_range=cmap_rng)
+#             self.c_debug(f"using {self.cset[0]} as color set for males")
+#         # only females present
+#         case 1 if "Female".casefold() in self.df["Sex"].values:
+#             plot_colors = CmapSet(
+#                 self.cset[1], data_array, cmap_range=cmap_rng)
+#             self.c_debug(f"using {self.cset[1]} as color set for females")
+#         case _:  # default to plasma cmap in any other case
+#             plot_colors = CmapSet(
+#                 self.cset[0], data_array, cmap_range=cmap_rng)
+#             self.c_debug(f"using {self.cset[1]} as default color set")
+
+class ColorSet:
+    """A class to handle color mapping for datasets."""
+
+    def __init__(self,
+                 color_name: str, metric_name: str,
+                 grouping_variable: str, group_name: str, data: pd.DataFrame | pd.Series | np.ndarray | None = None,
+                 cmap_range: tuple[float, float] = (0.4, 1), n_divisions: int = 151
+                 ):
+        self.name = color_name
+        self.metric_name = metric_name
+        self.grouping_variable = grouping_variable
+        if not isinstance(group_name, str):
+            raise ValueError("group_name must be a string.")
+        self.group_name = group_name
+        self.data = self._filter_data(data.copy())
+        self.cmap_range = cmap_range if cmap_range else (0, 1)
+        # get colormap based on name and range given
+        self.cmap = cm.get_cmap(color_name)
+        self.divcmap = ListedColormap(self.cmap(np.linspace(
+            self.cmap_range[0], self.cmap_range[1], n_divisions)), name=f"{color_name}_divided")
+        self.colors = self._generate_colors()
+
+    def _filter_data(self, data: pd.DataFrame | pd.Series | np.ndarray | None = None):
+        """Filters the data to the relevant group."""
+        if not isinstance(data, (pd.DataFrame, pd.Series, np.ndarray)):
+            return data
+        data = data[data[self.grouping_variable] == self.group_name][self.metric_name] if isinstance(
+            data, pd.DataFrame) else data[data[self.grouping_variable] == self.group_name]
+        # mo_print(
+        #     f'{self.grouping_variable} == {self.group_name} -> {len(data)} rows')
+        return data
+
+    def _generate_colors(self):
+        """Generates colors based on the colormap and data."""
+        if isinstance(self.data, pd.DataFrame):
+            values = self.data.select_dtypes(
+                include=[np.number]).values.flatten()
+        elif isinstance(self.data, pd.Series):
+            values = self.data.values.flatten()
+        elif isinstance(self.data, np.ndarray):
+            values = self.data.flatten()
+        else:
+            raise ValueError("Data must be a DataFrame or NumPy array.")
+
+        normed_values = (values - np.min(values)) / \
+            (np.max(values) - np.min(values))
+        return [colors.rgb2hex(self.cmap(v)) for v in normed_values]
+
+    def _map_colors(self, data: pd.DataFrame | np.ndarray | None = None):
+        """Maps colors to groups in a DataFrame or NumPy array."""
+        if data is None:
+            data = self.data
+        if isinstance(data, pd.DataFrame):
+            return data.apply(lambda x: self.colors[x.name], axis=1)
+        elif isinstance(data, np.ndarray):
+            return np.array([self.colors[i] for i in range(len(data))])
+        else:
+            raise ValueError("Data must be a DataFrame or NumPy array.")
+
+    def _blend_colors(self, other_color_set: 'ColorSet', split: bool = True):
+        """Blends this color set with another ColorSet."""
+        if not isinstance(other_color_set, ColorSet):
+            raise ValueError(
+                "other_color_set must be an instance of ColorSet.")
+
+        top_half = cm.get_cmap(self.name, 1024)
+        bottom_half = cm.get_cmap(other_color_set.name, 1024)
+
+        if split:
+            new_colors = np.vstack(
+                (top_half(np.linspace(0, 0.5, 1024)), bottom_half(np.linspace(0.5, 1, 1024))))
+        else:
+            new_colors = np.vstack(
+                (
+                    top_half(np.linspace(
+                        1 - self.cmap_range[1], 1 - self.cmap_range[0], 1024)),
+                    bottom_half(np.linspace(
+                        self.cmap_range[0], self.cmap_range[1], 1024)),
+                )
             )
-        )
-    if not name:
-        name = f"blended_{self.color_choice}"
-    blended_cmap = ListedColormap(newcolors, name=name)
-    self.blended_cmap = blended_cmap
+        return ListedColormap(new_colors, name=f"blended_{self.name}_{other_color_set.name}")
+
+    def __repr__(self):
+        return f"ColorSet(name={self.name}, group_name={self.group_name}, cmap_range={self.cmap_range})"
 
 
 def show_colormap(cmap: Union[str, ListedColormap], label: str = "", figsize: Tuple[float, float] = (1.25, 0.25), font_family: str = "arial"):
